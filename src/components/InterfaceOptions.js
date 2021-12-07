d7b6310694fad8521446f2200abd6c41d485d2b3
NotEternal <cent1pede@protonmail.ch>
2021-12-07 11:15:51 +0400

    Cleanup

--- src/components/InterfaceOptions.js
+++ src/components/InterfaceOptions.js
@@ -15,9 +15,7 @@ export function InterfaceOptions(props) {
   const web3React = useWeb3React()
 
   const [notification, setNotification] = useState('')
-  const [storage, setStorage] = useState(
-    '0x8506bd1659f92e9c304a719dC850B597e15eD65b'
-  )
+  const [storage, setStorage] = useState('')
   const [storageIsCorrect, setStorageIsCorrect] = useState(false)
 
   const updateStorageContract = (event) => setStorage(event.target.value)
