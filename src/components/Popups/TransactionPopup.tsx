3189a7a661a153fa4609c91e883d53241d8987cc
Vitaliy Shulik <shulikvitalya@gmail.com>
2022-11-16 11:53:13 +0600

    add translates

--- src/components/Popups/TransactionPopup.tsx
+++ src/components/Popups/TransactionPopup.tsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import { AlertCircle, CheckCircle } from 'react-feather'
 import styled, { ThemeContext } from 'styled-components'
 import { useActiveWeb3React } from 'hooks'
+import { Trans } from 'react-i18next'
 import { TYPE } from 'theme'
 import { ExternalLink } from 'theme/components'
 import { getExplorerLink } from 'utils'
@@ -84,7 +85,9 @@ export default function TransactionPopup({
         <TYPE.body fontWeight={500}>{summary ?? 'Hash: ' + hash.slice(0, 8) + '...' + hash.slice(58, 65)}</TYPE.body>
         {showAddTokenButton && !!token && (
           <LinkButton theme={theme} onClick={() => addTokenToMetamask(token.tokenInfo)}>
-            {`Add ${token.tokenInfo.symbol} to Metamask`}
+            <Trans i18nKey="addTokenToMetamask" values={{ tokenSymbol: token.tokenInfo.symbol }}>
+              Add token to Metamask
+            </Trans>
           </LinkButton>
         )}
         {chainId && <ExternalLink href={getExplorerLink(chainId, hash, 'transaction')}>View in Explorer</ExternalLink>}
